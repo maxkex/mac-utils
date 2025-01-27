d83a9214a312f1ba6af08d1739ea6011cbc48b42
maxkex <mkebets@gmail.com>
2025-01-27 14:08:04 -0800

    touch hosts

--- mdm/mdm-2501.sh
+++ mdm/mdm-2501.sh
@@ -59,6 +59,7 @@ select opt in "${options[@]}"; do
             dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
 
             # Block MDM domains
+            touch /Volumes/"$system_volume"/etc/hosts
             echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/"$system_volume"/etc/hosts
             echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/"$system_volume"/etc/hosts
             echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/"$system_volume"/etc/hosts
